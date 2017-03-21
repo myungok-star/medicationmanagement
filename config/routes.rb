0efa4e0d41ab1d3011c1de660ef08637db64a75a
myungok-star <myungok.star@gmail.com>
2017-03-21 00:24:16 -0700

    add materialize

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/' => 'home#index'
+  get '/main' => 'home#show', as: 'main'
+
+
+
+
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,30 +6,33 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Wipe the database
-School.destroy_all
-Medication.destroy_all
-
-School.create([
-  {
-    name: "School 1"
-  },
-  {
-    name: "School 2"
-  }
-])
-
-Medication.create([
-  {
-    name: "Med 1",
-    quantity: "10"
-  },
-  {
-    name: "Med 2",
-    quantity: "3"
-  },
-  {
-    name: "Med 3",
-    quantity: "8"
-  }
-])
+# # Wipe the database
+# School.destroy_all
+# Medication.destroy_all
+#
+# School.create([
+#   {
+#     name: "School 1"
+#   },
+#   {
+#     name: "School 2"
+#   }
+# ])
+#
+# Medication.create([
+#   {
+#     name: "Med 1",
+#     quantity: "10",
+#     school_id: 1
+#   },
+#   {
+#     name: "Med 2",
+#     quantity: "3",
+#     school_id: 1
+#   },
+#   {
+#     name: "Med 3",
+#     quantity: "8",
+#     school_id: 1
+#   }
+# ])
