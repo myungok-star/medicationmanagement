1adacf045f4ed1913bcf21cffb69f7f13f4863d8
myungok-star <myungok.star@gmail.com>
2017-03-20 23:23:32 -0700

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Wipe the database
+School.destroy_all
+Medication.destroy_all
+
+School.create([
+  {
+    name: "School 1"
+  },
+  {
+    name: "School 2"
+  }
+])
+
+Medication.create([
+  {
+    name: "Med 1",
+    quantity: "10"
+  },
+  {
+    name: "Med 2",
+    quantity: "3"
+  },
+  {
+    name: "Med 3",
+    quantity: "8"
+  }
+])
